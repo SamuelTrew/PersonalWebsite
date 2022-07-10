33b7a2e5e51d19726a89578f1e4a5b2ff98b177d
Samuel Trew <samuel.trew@outlook.com>
2022-07-10 12:54:15 +0100

    Added crt effect and ducks

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js"
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg"
+import styles from "./App.module.css"
+import Icon from "./Icon"
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
-};
+   const grid = Array.from({length:5},(v,k)=>k+1).map(_ => <Icon />)
 
-export default App;
+   return (
+      <div class={styles.crt}>
+      <div class={styles.refresh}/>
+
+      <div class={styles.App}>
+         {grid}
+      </div>
+      </div>
+   )
+}
+
+export default App
